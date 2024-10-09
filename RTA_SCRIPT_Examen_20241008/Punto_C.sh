6840427ce62637b05b3211c59a8f2d6b742a0858
root <root@Barahona>
2024-10-09 23:58:34 +0000

    feat: agregue el punto c

--- RTA_SCRIPT_Examen_20241008/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_C.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
-# Desmontar particiones del disco /dev/sdc, excepto /dev/sdc4
+LISTA=$1
+USER_HASH=$2
+HASH=$(sudo grep -i $USER_HASH /etc/shadow | awk -F ':' '{print $2}')
+echo "clave:"
+echo "$HASH"
 
-for i in {1..11}; do
-  if [ "$i" -ne 4 ]; then
-    # Definir la ruta de montaje según el índice
-    if [ "$i" -le 3 ]; then
-      MOUNT_POINT="/home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Barahona/EXAMENES-UTN/alumno_1/parcial_$i"
-    elif [ "$i" -le 7 ]; then
-      MOUNT_POINT="/home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Barahona/EXAMENES-UTN/alumno_2/parcial_$((i-4))"
-    elif [ "$i" -le 10 ]; then
-      MOUNT_POINT="/home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Barahona/EXAMENES-UTN/alumno_3/parcial_$((i-7))"
-    elif [ "$i" -eq 11 ]; then
-      MOUNT_POINT="/home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Barahona/EXAMENES-UTN/profesores"
-    fi
-    
-    # Verificar si la partición está montada
-    if mount | grep -q "$MOUNT_POINT"; then
-      echo "Desmontando $MOUNT_POINT..."
-      sudo umount "$MOUNT_POINT"
+# Recorre cada línea del archivo LISTA, ignorando comentarios
+while IFS= read -r LINEA; do
+    # Ignorar líneas vacías o comentarios
+    [[ $LINEA =~ ^#.*$ ]] && continue
 
-      if [ $? -eq 0 ]; then
-        echo "$MOUNT_POINT desmontado correctamente."
-      else
-        echo "Error al desmontar $MOUNT_POINT."
-      fi
+    echo "linea: $LINEA"
+    echo
+
+    USUARIO=$(echo "$LINEA" | awk -F ',' '{print $1}')
+    GRUPO=$(echo "$LINEA" | awk -F ',' '{print $2}')
+
+    # Verificar si el grupo ya existe
+    if [ $(grep -c "^$GRUPO:" /etc/group) -eq 0 ]; then
+        echo "Creando grupo: $GRUPO"
+        sudo groupadd "$GRUPO"  # Corrección aquí
     else
-      echo "$MOUNT_POINT no está montado."
+        echo "El grupo $GRUPO ya existe."
     fi
-  else
-    echo "Saltando /dev/sdc4 (extendida)."
-  fi
-done
+
+    # Verificar si el usuario ya existe
+    if [ $(grep -c "^$USUARIO:" /etc/passwd) -eq 0 ]; then
+        echo "Creando usuario: $USUARIO"
+        sudo useradd -m -p "$HASH" -s /bin/bash -G "$GRUPO" "$USUARIO"
+else
+        echo "El grupo $GRUPO ya existe."
+    fi
+
+    # Verificar si el usuario ya existe
+    if [ $(grep -c "^$USUARIO:" /etc/passwd) -eq 0 ]; then
+        echo "Creando usuario: $USUARIO"
+        sudo useradd -m -p "$HASH" -s /bin/bash -G "$GRUPO" "$USUARIO"
+    else
+        echo "El usuario $USUARIO ya existe."
+    fi
+
+    echo "-------------------------------"
+done < "$LISTA"
+
+ 
 
